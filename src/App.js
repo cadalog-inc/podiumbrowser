f4ca9bf1a0c0a0c33323bdc1066c9785c8ddc36c
Andre V. Bursch <andrebursch@gmail.com>
2019-11-12 14:05:58 -0800

    initial and rough first attempt

--- src/App.js
+++ src/App.js
@@ -1,26 +1,70 @@
 import React from 'react';
+import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: []
+    };
+  }
+
+  componentDidMount() {
+    this.getItems();
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <table>
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Category Name</th>
+              <th>Category Title</th>
+              <th>File Size</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              this.state.items.map((item, index) => {
+                return (
+                  <tr key={index}>
+                    <td>{item.title}</td>
+                    <td>{item.category_name}</td>
+                    <td>{item.category_title}</td>
+                    <td>{this.formatFilesize(item.filesize)}</td>
+                  </tr>
+                );
+              })
+            }
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+
+  getItems() {
+    axios.get('filedatamin.json')
+    .then((response) => {
+      this.setState({
+        items: response.data.data
+      }, () => {
+        // console.log(this.state.items);
+      });
+    });
+  }
+
+  formatFilesize(bytes) {
+    const size = Math.round(bytes / Math.pow(1024, 2));
+    if(size < 1) {
+      return '< 1';
+    } else {
+      return size;
+    }
+  }
 }
 
 export default App;
