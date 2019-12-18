352ac7f25b479b424eb3e7be2773478203aa5ddf
Andre V. Bursch <andrebursch@gmail.com>
2019-12-17 16:47:35 -0800

    more additions

--- src/components/page.jsx
+++ src/components/page.jsx
@@ -133,6 +133,8 @@ export class Page extends React.Component {
 
                 {/* to do -- conditional rendering of favorites section here */}
                 {categoryId === 1 ? <FavoritesSection>
+                    {/* add in favorites here as a props */}
+                    {/* favorites={this.props.favorites} */}
 
                 </FavoritesSection>
                     : null}
