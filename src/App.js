0d413d616b76947ece40347d51b5db0cfd7475c3
Anton Bursch <antonphd@gmail.com>
2020-09-10 17:37:05 -0700

    quick fix to cached data

--- src/App.js
+++ src/App.js
@@ -571,7 +571,7 @@ class App extends React.Component {
     }
 
     getCachedCategories() {
-        axios.get(`categories.json`)
+        axios.get(`https://v4.pdm-plants-textures.com/categories.json`)
             .then((response) => {
                 this.setState({
                     categories: Category.fromArray(response.data)
@@ -585,7 +585,7 @@ class App extends React.Component {
     }
 
     getCachedItems() {
-        axios.get(`items.json`)
+        axios.get(`https://v4.pdm-plants-textures.com/items.json`)
             .then((response) => {
                 this.setState({
                     items: Item.fromArray(response.data)
@@ -599,7 +599,7 @@ class App extends React.Component {
     }
 
     getCachedRelationships() {
-        axios.get(`relationships.json`)
+        axios.get(`https://v4.pdm-plants-textures.com/relationships.json`)
             .then((response) => {
                 this.setState({
                     relationships: Relationship.fromArray(response.data)
